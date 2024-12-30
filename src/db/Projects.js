2162b1dc589b5c70bcec30ae30a91fe0c9aa1ca1
hamzayounis106 <hamzayounis106@gmail.com>
2024-12-30 05:40:51 +0500

    Normalize all the line endings

--- src/db/Projects.js
+++ src/db/Projects.js
@@ -15,16 +15,16 @@ const projectsData = [
   },
   {
     id: 2,
-    name: "Made Men Enterprise",
+    name: "Vertilux",
     description:
-      "Designed and developed a professional WordPress website for Made Men Enterprises, showcasing their expertise in multi-family workforce housing developments. The site features a clean and modern layout with user-friendly navigation, compelling content that highlights the company's mission to spur economic growth, and integrated functionality for seamless consultation requests. This project demonstrates my ability to create impactful websites that align with a client’s vision and goals.",
-    imageUrl: "/projects/Made Men Enterprise.png",
+      "Vertilux is a premier provider of innovative window covering solutions, offering a diverse range of products like roller shades, Neolux dual shades, vertical blinds, and automation options. With over 35 years of expertise, Vertilux combines style and functionality to transform living and working spaces, catering to architects, designers, and homeowners. Their commitment to quality, service, and cutting-edge design ensures every project is executed with precision and creativity.",
+    imageUrl: "/projects/Vertilux.png",
     tags: ["Wordpress", "Elementor"],
-    liveUrl: "https://mademenenterprises.com/",
+    liveUrl: "https://aep.tdd.mybluehost.me/",
     isFeatured: true,
     colors: {
-      primary: "#6D95FC",
-      secondary: "#6D95FC",
+      primary: "#aa2d35",
+      secondary: "#fff",
     },
   },
   {
@@ -37,8 +37,8 @@ const projectsData = [
     liveUrl: "https://mademenenterprises.com/",
     isFeatured: true,
     colors: {
-      primary: "#ffffff",
-      secondary: "#046bd2",
+      primary: "#046bd2",
+      secondary: "#fff",
     },
   },
 ];
